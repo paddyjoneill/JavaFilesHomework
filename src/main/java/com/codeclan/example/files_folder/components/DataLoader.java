3199f090b4ad668df565b3877507acff97befa53
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-02-26 09:08:32 +0000

    saving to database :)

--- src/main/java/com/codeclan/example/files_folder/components/DataLoader.java
+++ src/main/java/com/codeclan/example/files_folder/components/DataLoader.java
@@ -9,7 +9,9 @@ import com.codeclan.example.files_folder.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
